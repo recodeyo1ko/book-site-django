1e6dff620bb9fed540aa3d93d6f216eebdd7b7c4
recodeyo1ko <kuronekoryohei@gmail.com>
2024-05-11 00:15:52 +0900

    topからindex画面への遷移

--- src/book_site/urls.py
+++ src/book_site/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('books/', views.book_index, name='book_index'),
+    path('books/<int:book_id>/', views.book_show, name='book_show'),
 ]
--- src/book_site/views.py
+++ src/book_site/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home/top.html')
 
+def book_index(request):
+    return render(request, 'books/index.html')
+
+def book_show(request):
+    return render(request, 'books/show.html')
