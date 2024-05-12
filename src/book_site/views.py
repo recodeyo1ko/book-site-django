1c128ad413a82ede7f65f3b826ed5254d787447c
recodeyo1ko <kuronekoryohei@gmail.com>
2024-05-12 23:12:48 +0900

    Bookデータの挿入、投稿、投稿を一覧画面で確認

--- src/book_site/views.py
+++ src/book_site/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from .models import Book
 
 # Create your views here.
 
@@ -6,7 +8,19 @@ def home(request):
     return render(request, 'home/top.html')
 
 def book_index(request):
-    return render(request, 'books/index.html')
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
 
 def book_show(request):
     return render(request, 'books/show.html')
+
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'books/index.html')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
