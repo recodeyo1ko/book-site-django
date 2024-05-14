c926fa75e694d72b8091a46da77175c180ffbdfd
recodeyo1ko <kuronekoryohei@gmail.com>
2024-05-14 21:41:18 +0900

    詳細画面追加

--- src/book_site/urls.py
+++ src/book_site/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('books/', views.book_index, name='book_index'),
     path('books/<int:book_id>/', views.book_show, name='book_show'),
+    path('books/<int:book_id>/edit/', views.book_edit, name='book_edit'),
     path('books/create/', book_create, name='book_create'),
 ]
--- src/book_site/views.py
+++ src/book_site/views.py
@@ -12,9 +12,12 @@ def book_index(request):
     form = BookForm()
     return render(request, 'books/index.html', {'books': books, 'form': form})
 
-def book_show(request):
-    return render(request, 'books/show.html')
+def book_show(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/show.html' , {'book': book})
 
+def book_edit(request):
+    return render(request, 'books/edit.html' )
 
 def book_create(request):
     if request.method == 'POST':
@@ -26,3 +29,18 @@ def book_create(request):
     books = Book.objects.all()
     form = BookForm()
     return render(request, 'books/index.html', {'books': books, 'form': form})
+
+def book_show(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/show.html', {'book': book})
+
+def book_edit(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_show', book_id=book.id)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/edit.html', {'form': form})
